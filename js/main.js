a2aebe242b3d99b68320fa226da7e1c49b9ab4a4
altkaye <khtkhk@gmail.com>
2015-10-28 23:05:23 +0900

    testing some framework and functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 (function() {
     console.log("hello world!");
+    var sb = {};
+    window.sb = sb;
+
+    sb.TAG = 'sho-giban';
+
+    sb.Shogiban = document.registerElement(sb.TAG, {
+        prototype: Object.create(HTMLElement.prototype, {
+            createdCallback: {
+                value: function() {
+                    this.innerHTML = "<span>sho-giban</span>";
+
+                    this.style.backgroundColor = "#393";
+                }
+            }
+        })
+    });
 })();
