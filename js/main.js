e9898a43cc3fe1223e268795b70bd258238e0d42
relva.org <info@relva.org>
2015-10-28 23:28:23 +0900

    testing phina.js

--- js/main.js
+++ js/main.js
@@ -12,8 +12,31 @@
                     this.innerHTML = "<span>sho-giban</span>";
 
                     this.style.backgroundColor = "#393";
+                    var canvas = document.createElement("canvas");
+                    this.appendChild(canvas);
+                    var ctx = canvas.getContext("2d");
+                    ctx.fillText("foobar", 100, 100);
+
+                    this.canvas = canvas;
                 }
             }
         })
+    });
+
+    phina.main(function() {
+        var shogibans = document.getElementsByTagName("sho-giban");
+        for (var i = 0; i < shogibans.length; i++) {
+            (function(dom) {
+                var param = {
+                    width:dom.getAttribute("width") || 300,
+                    height:dom.getAttribute("height") || 300,
+                    domElement:dom.canvas,
+                    fit:false
+                };
+                var app = phina.display.CanvasApp(param);
+                app.run();
+                phina.display.StarShape().addChildTo(app.currentScene);
+            })(shogibans[i]);
+        }
     });
 })();
